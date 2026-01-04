489fe931e28f674de6ef1613b5917fc2867c8346
Ar3enn <ar3enn@gmail.com>
2026-01-04 15:41:47 +0300

    Fixed indentation errors in textnode.py

--- src/main.py
+++ src/main.py
@@ -1,1 +1,8 @@
-print("Hello world")
+from textnode import TextNode, TextType
+
+def main():
+    dummy_node = TextNode("This is a text node", TextType.BOLD, "https://www.github.com")
+    print(dummy_node)
+
+if __name__ == "__main__":
+    main()
--- src/textnode.py
+++ src/textnode.py
@@ -14,12 +14,12 @@ class TextNode:
         self.text_type = text_type
         self.url = url
         
-def __eq__(self, other):
-    return (
+    def __eq__(self, other):
+        return (
         self.text == other.text 
         and self.text_type == other.text_type 
         and self.url == other.url
         )
 
-def __repr__(self):
-    return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
+    def __repr__(self):
+        return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
