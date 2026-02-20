98565816f042f760975d061d3b774deb0bc97453
Ar3enn <ar3enn@gmail.com>
2026-02-20 03:59:30 +0300

    Copy static

--- src/main.py
+++ src/main.py
@@ -1,8 +1,27 @@
+import os
+import shutil
+
 from textnode import TextNode, TextType
 
+
+def copy_static(src, dst):
+    if os.path.exists(dst):
+        shutil.rmtree(dst)
+    os.mkdir(dst)
+
+    for item in os.listdir(src):
+        src_path = os.path.join(src, item)
+        dst_path = os.path.join(dst, item)
+        if os.path.isfile(src_path):
+            print(f"Copying {src_path} -> {dst_path}")
+            shutil.copy(src_path, dst_path)
+        else:
+            copy_static(src_path, dst_path)
+
+
 def main():
-    dummy_node = TextNode("This is a text node", TextType.BOLD, "https://www.github.com")
-    print(dummy_node)
+    copy_static("static", "public")
+
 
 if __name__ == "__main__":
     main()
